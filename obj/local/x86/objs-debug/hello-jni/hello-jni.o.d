4eb02bea96a2004ac78d79d284803b8b3c797d82
mathpierdev305 <mathpierdev305@gmail.com>
2015-03-15 21:13:48 -0400

    initial commit

---
+++ obj/local/x86/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/x86/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/string.h \
+ C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/cdefs.h \
+ C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/cdefs_elf.h \
+ C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/android/api-level.h \
+ C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/malloc.h \
+ C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/jni.h
+
+C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/string.h:
+
+C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/cdefs.h:
+
+C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/sys/cdefs_elf.h:
+
+C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/android/api-level.h:
+
+C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/malloc.h:
+
+C:/Android/android-ndk-r10d/platforms/android-14/arch-x86/usr/include/jni.h:
---
+++ obj/local/x86_64/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,22 @@
+obj/local/x86_64/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/string.h \
+ C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/sys/cdefs.h \
+ C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/sys/cdefs_elf.h \
+ C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/android/api-level.h \
+ C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/malloc.h \
+ C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/xlocale.h \
+ C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/jni.h
+
+C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/string.h:
+
+C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/sys/cdefs.h:
+
+C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/sys/cdefs_elf.h:
+
+C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/android/api-level.h:
+
+C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/malloc.h:
+
+C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/xlocale.h:
+
+C:/Android/android-ndk-r10d/platforms/android-21/arch-x86_64/usr/include/jni.h:
